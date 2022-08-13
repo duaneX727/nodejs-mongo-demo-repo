bb80828e1075a1658974639aa4d04527d357f41c
Marlon Mitchell <mdmitch7@gmail.com>
2022-08-13 02:51:03 -0400

    Add a npm start script to package.json. Also, refactored genres.test.js

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -4,7 +4,7 @@ const request = require('supertest');
 const {Genre} = require('../../models/genre');
 const {User} = require('../../models/user');
 const mongoose = require('mongoose');
-const config = require('config');
+//const config = require('config');
 
 let server;
 
@@ -13,12 +13,12 @@ describe('/api/genres', () => {
      server = require('../../app');});
      
   afterEach(async () => {
-     server.close().unref();
+     await server.close().unref();
      await Genre.deleteMany({});
   });
   describe('GET /', () => {
-    it('Server config exist', () => {
-    expect(config.get('db.host')).toBe('mongodb://localhost/vidlyDB_tests');});
+    // it('Server config exist', () => {
+    // expect(config.get('db.host')).toBe('mongodb://localhost/vidlyDB_tests');});
     it('should return all genres', async () => {
       
      await Genre.collection.insertMany([{name: 'genre1'},{name: 'genre2'}]);
@@ -44,54 +44,53 @@ describe('/api/genres', () => {
      });
   });
   describe('POST /', () => {
-     it('should return 401 if client is not logged in', async () => {
-       const res = await request(server)
+     // Define the happy path, and then in each test, we change 
+    // one parameter that clearly aligns with the name of the 
+    // test. 
+    let token; 
+    let name; 
+    const exec = async () => {
+      return await request(server)
         .post('/api/genres')
-        .send({ name: 'genre1' });
-
-       expect(res.status).toBe(401);
+        .set('x-auth-token', token)
+        .send({ name });
+    };
+    beforeEach(() => {
+      token = new User().generateAuthToken();      
+      name = 'genre1'; 
+    });
+    it('should return 401 if client is not logged in', async () => {
+      token = ''; 
+      const res = await exec();
+        
+      expect(res.status).toBe(401);
      });
      it('should return 400 if genre is less than 5 characters', async () => {
-      const token = new User().generateAuthToken();
-      const res = await request(server)
-       .post('/api/genres')
-       .set('x-auth-token', token)
-       .send({ name: '1234' });
+      name = '1234';
+    
+      const res = await exec();
 
       expect(res.status).toBe(400);
      });
      it('should return 400 if genre is greater than 50 characters', async () => {
-      const token = new User().generateAuthToken();
-      const name = new Array(52).join('a');
-      const res = await request(server)
-       .post('/api/genres')
-       .set('x-auth-token', token)
-       .send({ name: name });
+      
+      name = new Array(52).join('a');
+      const res = await exec();
 
       expect(res.status).toBe(400);
      });
-  });
- 
-  it('should save the genre if it is valid', async () => {
-    const token = new User().generateAuthToken();
-    const res = await request(server)
-      .post('/api/genres')
-      .set('x-auth-token', token)
-      .send({ name:'genre1' });
+    it('should save the genre if it is valid', async () => {
+      await exec();
 
-      const genre = await Genre.find({name:'genre1'});
-      //expect(genre === '').toBeTruthy();
-    
-    expect(genre).not.toBeNull();
-  });
-  it('should return genre if it is valid', async () => {
-    const token = new User().generateAuthToken();
-    const res = await request(server)
-      .post('/api/genres')
-      .set('x-auth-token', token)
-      .send({ name: 'genre1' });
-    
-    expect(res.body).toHaveProperty('_id');
-    expect(res.body).toHaveProperty('name','genre1');
+      const genre = await Genre.find({ name: 'genre1' });
+
+      expect(genre).not.toBeNull();
+    });
+    it('should return genre if it is valid', async () => {
+      const res = await exec();
+
+      expect(res.body).toHaveProperty('_id');
+      expect(res.body).toHaveProperty('name','genre1');
+    });
   });
 });
